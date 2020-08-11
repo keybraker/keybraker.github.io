151e2668a86c2fc8d8f3bcfdf93eea0bd94e00a8
keybraker <iantsiakkas@gmail.com>
2020-08-12 02:17:48 +0300

    fixed oauth2 discord communication

--- assets/js/discord_login.js
+++ assets/js/discord_login.js
@@ -1,10 +1,75 @@
+
 window.onload = () => {
-  console.log('inside');
+  const auth_code = window.location.href
+    .slice(("http://www.localhost:4000/premium/?code=").length, window.location.href.length);
+  console.log('inside, ', auth_code);
+
+
+  const data = {
+    client_id: '704400876860735569',
+    client_secret: '',
+    grant_type: 'authorization_code',
+    redirect_uri: 'http://www.localhost:4000/premium/',
+    code: auth_code,
+    scope: 'identify',
+  };
+
+//   fetch('https://discordapp.com/api/oauth2/token', {
+//     method: 'POST',
+//     body: new URLSearchParams(data),
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//   })
+//     .then(res => res.json())
+//     .then(console.log)
+//     .error(console.log);
+
+// fetch('https://discord.com/api/oauth2/token', {
+//     method: 'POST',
+//     body: new URLSearchParams(data),
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//   })
+//     .then(res => res.json())
+//     .then(console.log)
+//     .error(console.log);
+
+const accessToken = "fHh8QzvA7qKrV1nxyuLJaBODWgHeAC";
+const tokenType = "Bearer";
+
+fetch('https://discordapp.com/api/users/@me', {
+      headers: {
+        authorization: `${tokenType} ${accessToken}`
+      }
+    })
+      .then(res => res.json())
+      .then(response => {
+        const { username, discriminator } = response;
+        console.log('response :>> ', response);
+        console.log('username: ', username);
+        console.log('discriminator: ', discriminator);
+      })
+      .catch(console.error);
+
+fetch('https://discordapp.com/api/users/@me/guilds', {
+      headers: {
+        authorization: `${tokenType} ${accessToken}`
+      }
+    })
+      .then(res => res.json())
+      .then(response => {
+        const { username, discriminator } = response;
+        console.log('response :>> ', response);
+        console.log('username: ', username);
+        console.log('discriminator: ', discriminator);
+      })
+      .catch(console.error);
+
+
+
   const fragment = new URLSearchParams(window.location.hash.slice(1));
-  console.log('window: ', window);
-  console.log('window.location: ', window.location);
-  console.log('window.location.hash: ', window.location.hash);
-  console.log('fragment: ', fragment);
 
   if (fragment.has("access_token")) {
     const accessToken = fragment.get("access_token");
