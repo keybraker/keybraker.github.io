99d6a671a69af1beacddc20bb9f015fbb9c01bf5
keybraker <iantsiakkas@gmail.com>
2020-09-12 01:43:44 +0300

    updated documentation and finished with use log in  with discord

--- assets/js/discord_login.js
+++ assets/js/discord_login.js
@@ -1,98 +1,88 @@
-
 window.onload = () => {
-  const auth_code = window.location.href
-    .slice(("http://www.localhost:4000/premium/?code=").length, window.location.href.length);
-  console.log('inside, ', auth_code);
+  const token_type = "Bearer";
+  const access_code = window.location.search.slice(6, window.location.href.length);
+  const access_token = localStorage.getItem('access_token');
+  console.log('window.sessionStorage: ', window.sessionStorage);
+  console.log('window.localStorage: ', window.localStorage);
+  if (!access_token && !access_code) {
+    console.log('passed 1');
+    display_button();
+  } else if (!access_token && access_code) {
+    console.log('passed 2');
+    get_access_token(access_code);
+  }
 
+  fetch_user_data(token_type, access_token);
+  fetch_user_guilds(token_type, access_token);
+}
 
+const display_button = () => {
+  document.getElementById('login').style.display = 'block';
+}
+
+const get_access_token = (access_code) => {
   const data = {
     client_id: '704400876860735569',
     client_secret: '',
     grant_type: 'authorization_code',
     redirect_uri: 'http://www.localhost:4000/premium/',
-    code: auth_code,
+    code: access_code,
     scope: 'identify',
   };
 
-//   fetch('https://discordapp.com/api/oauth2/token', {
-//     method: 'POST',
-//     body: new URLSearchParams(data),
-//     headers: {
-//       'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//   })
-//     .then(res => res.json())
-//     .then(console.log)
-//     .error(console.log);
-
-// fetch('https://discord.com/api/oauth2/token', {
-//     method: 'POST',
-//     body: new URLSearchParams(data),
-//     headers: {
-//       'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//   })
-//     .then(res => res.json())
-//     .then(console.log)
-//     .error(console.log);
-
-const accessToken = "fHh8QzvA7qKrV1nxyuLJaBODWgHeAC";
-const tokenType = "Bearer";
-
-fetch('https://discordapp.com/api/users/@me', {
-      headers: {
-        authorization: `${tokenType} ${accessToken}`
-      }
+  fetch('https://discordapp.com/api/oauth2/token', {
+    method: 'POST',
+    body: new URLSearchParams(data),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+    .then(res => res.json())
+    .then(response => {
+      const { access_token, expires_in, refresh_token, scope, token_type  } = response;
+      localStorage.setItem('access_token', access_token);
+      localStorage.setItem('expires_in', expires_in);
+      localStorage.setItem('refresh_token', refresh_token);
+      localStorage.setItem('scope', scope);
+      localStorage.setItem('token_type', token_type);
+      fetch_user_data(token_type, access_token);
+      fetch_user_guilds(token_type, access_token);
     })
-      .then(res => res.json())
-      .then(response => {
-        const { username, discriminator } = response;
-        console.log('response :>> ', response);
-        console.log('username: ', username);
-        console.log('discriminator: ', discriminator);
-      })
-      .catch(console.error);
+    .error(console.log);
+}
 
-fetch('https://discordapp.com/api/users/@me/guilds', {
-      headers: {
-        authorization: `${tokenType} ${accessToken}`
-      }
+const fetch_user_data = (token_type, access_token) => {
+  console.log(`fetch_user_data: ${token_type} ${access_token}`);
+  fetch('https://discordapp.com/api/users/@me', {
+    headers: {
+      authorization: `${token_type} ${access_token}`
+    }
+  })
+    .then(res => res.json())
+    .then(response => {
+      const { avatar, discriminator, flags, id, locale, mfa_enabled, public_flags, username } = response;
+      console.log('storing data for username username: ', username);
+      sessionStorage.setItem('avatar', avatar);
+      sessionStorage.setItem('discriminator', discriminator);
+      sessionStorage.setItem('flags', flags);
+      sessionStorage.setItem('id', id);
+      sessionStorage.setItem('locale', locale);
+      sessionStorage.setItem('mfa_enabled', mfa_enabled);
+      sessionStorage.setItem('public_flags', public_flags);
+      sessionStorage.setItem('username', username);
     })
-      .then(res => res.json())
-      .then(response => {
-        const { username, discriminator } = response;
-        console.log('response :>> ', response);
-        console.log('username: ', username);
-        console.log('discriminator: ', discriminator);
-      })
-      .catch(console.error);
+    .catch(console.error);
+}
 
-
-
-  const fragment = new URLSearchParams(window.location.hash.slice(1));
-
-  if (fragment.has("access_token")) {
-    const accessToken = fragment.get("access_token");
-    const tokenType = fragment.get("token_type");
-    console.log('accessToken: ', accessToken);
-    console.log('tokenType: ', tokenType);
-
-    fetch('https://discordapp.com/api/users/@me', {
-      headers: {
-        authorization: `${tokenType} ${accessToken}`
-      }
+const fetch_user_guilds = (token_type, access_token) => {
+  fetch('https://discordapp.com/api/users/@me/guilds', {
+    headers: {
+      authorization: `${token_type} ${access_token}`
+    }
+  })
+    .then(res => res.json())
+    .then(response => {
+      sessionStorage.setItem('guilds', response);
     })
-      .then(res => res.json())
-      .then(response => {
-        const { username, discriminator } = response;
-        document.getElementById('info').innerText += ` ${username}#${discriminator}`;
-        console.log('username: ', username);
-        console.log('discriminator: ', discriminator);
-      })
-      .catch(console.error);
-  }
-  else {
-    console.log('could not log in');
-    document.getElementById('login').style.display = 'block';
-  }
+    .catch(console.error);
 }
