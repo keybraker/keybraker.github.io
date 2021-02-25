d0b9a9bb4cffc8f79feaa04a1dd13287fb725ac9
Ioannis Tsiakkas <iantsiakkas@gmail.com>
2021-02-25 11:43:00 -0800

    test of new page

--- assets/js/paypal_payment.js
+++
@@ -1,8 +0,0 @@
-const selectGuild = (boost_guild_id, boost_guild_name, boost_guild_cost, boost_guild_period) => {
-  console.log(`Upgrade ${boost_guild_id} for boost_guild_cost ${boost_guild_cost}`);
-  sessionStorage.setItem('boost_guild_id', boost_guild_id);
-  sessionStorage.setItem('boost_guild_name', boost_guild_name);
-  sessionStorage.setItem('boost_guild_cost', boost_guild_cost);
-  sessionStorage.setItem('boost_guild_period', boost_guild_period);
-  location.reload();
-}
