d5427acc1cb8b5e5aeebd9a1caaf05a52b61905e
Ioannis Tsiakkas <iantsiakkas@gmail.com>
2021-03-14 06:23:06 -0700

    updated attributes

--- 248fa3e2.fd822ae8.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{94:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"0.6.4","permalink":"/blog/0.6.4"},{"title":"0.6.3","permalink":"/blog/0.6.3"},{"title":"0.6.0","permalink":"/blog/0.6.0"},{"title":"0.5.7","permalink":"/blog/0.5.7"},{"title":"0.5.0","permalink":"/blog/0.5.0"}]}')}}]);
---
+++ 71e0d24e.b2537641.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[55],{129:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"bot","name":"bot","count":8,"permalink":"/blog/tags/bot"}')}}]);
