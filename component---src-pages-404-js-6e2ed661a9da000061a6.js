e2b2a70b1c0d9fb73d2d125ce7b992dc1fd3469b
Ioannis Tsiakkas <ioannis.tsiakkas@fairlo.se>
2022-03-03 17:28:59 +0200

    removed a lot of images

--- component---src-pages-404-js-6e2ed661a9da000061a6.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,t,e){"use strict";e.r(t);var c=e("q1tI"),u=e("YwZP");t.default=function(){return Object(c.useEffect)((function(){Object(u.navigate)("/")})),null}}}]);
-//# sourceMappingURL=component---src-pages-404-js-6e2ed661a9da000061a6.js.map
