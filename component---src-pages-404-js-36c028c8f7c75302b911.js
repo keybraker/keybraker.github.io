f136e1074b8fcc946d7d58adb01a57eb41a15e65
keybraker <keybraker@users.noreply.github.com>
2022-08-06 09:39:54 +0000

    Deploying to gh-pages from @ keybraker/keybraker.github.io@bfba3a00efae0fe910e3800823700663c385d898 🚀

---
+++ component---src-pages-404-js-36c028c8f7c75302b911.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,t,e){"use strict";e.r(t);var c=e("q1tI"),u=e("YwZP");t.default=function(){return Object(c.useEffect)((function(){Object(u.navigate)("/")})),null}}}]);
+//# sourceMappingURL=component---src-pages-404-js-36c028c8f7c75302b911.js.map
