a47cf63822fcbe313827e539fb79f84679e4e8a5
Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-02-17 15:32:50 +0200

    Make landing page look like an email

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,100 @@
 import React from "react";
 import styled from "styled-components";
 // import loadable from '@loadable/component';
+import { HiOutlineReply } from "@react-icons/all-files/hi/HiOutlineReply";
 
 import Layout from "./../components/Layout";
+import dayjs from "dayjs";
 // const HeadScene = loadable(() => import('./../components/HeadScene'));
 
 import Confetti from "react-confetti";
 
 const StyledContainer = styled.div`
-  height: 350px;
+  border: 1px solid black;
+  border-radius: 6px;
+  padding: 10px;
 
   @media (min-width: 520px) {
-    height: 400px;
+    // height: 400px;
   }
 
   @media (min-width: 1400px) {
-    height: 500px;
+    // height: 500px;
+  }
+`;
+
+const StyledHorizontalLine = styled.div`
+  margin-top: 16px;
+  margin-bottom: 16px;
+  border-top: 1px dashed gray;
+`;
+
+const StyleHeader = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+
+const StyledReplyWrapper = styled.div`
+  font-family: "Archivo", sans-serif;
+  font-size: 16px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+  margin-top: 18px;
+`;
+
+const StyledReplyButton = styled.a`
+  font-family: "Archivo", sans-serif;
+  font-size: 18px;
+  border: none;
+  background: transparent;
+  border: 1px solid black;
+  border-radius: 6px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 6px;
+  padding: 6px 10px;
+  margin-top: 4px;
+  color: black;
+
+  &:hover {
+    background-color: #e8e3e3;
+    color: black;
   }
 `;
 
 const StyledSectionHeading = styled.span`
   font-family: "Archivo", sans-serif;
   font-style: italic;
-  font-size: 24px;
+  font-size: 20px;
   margin-bottom: 0;
   color: #000;
   text-decoration: none;
 `;
 
-const StyledSectionDate = styled.span`
+const StyleHeaderFlexCol = styled.div`
   font-family: "Archivo", sans-serif;
-  font-style: italic;
+  // font-style: italic;
   font-size: 16px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: 0;
+  color: black;
+  text-decoration: none;
+`;
+
+const StyleHeaderFlexRow = styled.div`
+  font-family: "Archivo", sans-serif;
+  // font-style: italic;
+  font-size: 16px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: row;
   margin-bottom: 0;
   color: #9a9797;
   text-decoration: none;
@@ -53,34 +116,37 @@ function getWindowDimensions() {
 
 const { height, width } = getWindowDimensions();
 
-var MyDate = new Date();
-var date;
+const mail = "mailto:" + "iantsiakkas@gmail.com";
+const subject = "?subject=" + "Email%20Subject";
+const body = "&body=" + "Email%20Body";
 
-MyDate.setDate(MyDate.getDate());
-
-date =
-  ("0" + MyDate.getDate()).slice(-2) +
-  "/" +
-  ("0" + (MyDate.getMonth() + 1)).slice(-2) +
-  "/" +
-  MyDate.getFullYear();
+const date = dayjs().format("MMM DD, YYYY, HH:mm A");
 
 const Index = () => (
   <Layout>
     {/* <Confetti
       width={width}
       height={height}
-      gravity={0.005}
+      gravity={0.02}
       numberOfPieces={50}
       tweenDuration={500}
     /> */}
     <StyledContainer>
       {/* <HeadScene /> */}
+      <StyleHeader>
+        <StyleHeaderFlexCol>
+          <span>Ioannis Tsiakkas </span>
+          <span>to you</span>
+        </StyleHeaderFlexCol>
+        <StyleHeaderFlexRow>{date}</StyleHeaderFlexRow>
+      </StyleHeader>
+      <StyledHorizontalLine></StyledHorizontalLine>
       <StyledSectionHeading>
         Hello,<br></br>
         <br></br>
-        I'm a Software Engineer with experience in both frontend and backend
-        development.<br></br>
+        I'm a Software Engineer with experience in building web application. I
+        have experience in both the backend infrustructure and the frontend.
+        <br></br>
         Currently working as a backend developer at{" "}
         <a href="https://fairlo.se/" rel="noopener noreferrer" target="_blank">
           Fairlo
@@ -89,9 +155,13 @@ const Index = () => (
         <br></br>
         Kindly,<br></br>
         Ioannis Tsiakkas<br></br>
-        <StyledSectionDate>Sent {date}</StyledSectionDate>
       </StyledSectionHeading>
     </StyledContainer>
+    <StyledReplyWrapper>
+      <StyledReplyButton href={`${mail}${subject}${body}`}>
+        <HiOutlineReply /> Reply
+      </StyledReplyButton>
+    </StyledReplyWrapper>
   </Layout>
 );
 
