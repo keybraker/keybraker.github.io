55ad2aedc47ff2ad5f6b208132f6ec42a3385d2f
Keybraker <iantsiakkas@gmail.com>
2023-02-18 21:02:46 +0200

    Update next config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: isProd ? "/your-github-repo-name/" : "",
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
