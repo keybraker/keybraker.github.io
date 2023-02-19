a422b6eb9f44fdc479fd1bf6f06009e44a1fd058
Keybraker <iantsiakkas@gmail.com>
2023-02-19 21:04:15 +0200

    Fix footer

--- src/abilities/footer.tsx
+++ src/abilities/footer.tsx
@@ -8,10 +8,12 @@ export default function Footer() {
     <>
       <div className="border border-gray-300 mt-4"></div>
       <div className="flex flex-col xs:flex-row justify-between items-center xs:items-stretch">
-        <span className="w-full flex flex-row justify-between text-sm hover:underline">
+        <span className="w-full flex flex-row justify-between text-sm">
           <Image src={signature} alt="The source of truth" width={84} />
           <a rel="noopener noreferrer" target="_blank" href={repo}>
-            <span>Created with by me witn NextJS and Tailwind</span>
+            <span className="hover:underline">
+              Created by me with NextJS and Tailwind.
+            </span>
           </a>
         </span>
       </div>
