82f384deb8c9f02787d2e06d7b154bad983c1945
Keybraker <iantsiakkas@gmail.com>
2023-02-19 18:28:27 +0200

    Update how education is displayed

--- src/abilities/education.tsx
+++ src/abilities/education.tsx
@@ -6,17 +6,15 @@ const education: ShowcaseType[] = [
     start: "Sep 2013",
     end: "Jan 2019",
     title: "Bachelor of Computer Science Degree",
-    info: "GPA: 3.00 B",
-    position:
-      '<a href="https://www.csd.uoc.gr/CSD/index.jsp?lang=en" rel="noopener noreferrer" target="_blank">University of Crete</a>',
+    info: '<a href="https://www.sd.uoc.gr/CSD/index.jsp?lang=en" rel="noopener noreferrer" target="_blank">University of Crete</a>',
+    position: "GPA: 3.00 B",
   },
   {
     start: "Mar 2018",
     end: "Jan 2019",
     title: "<i>“A versatile machine learning API for GPGPUs”</i>",
-    position:
-      '<a href="https://www.ics.forth.gr/discs/r" rel="noopener noreferrer" target="_blank">DSC</a> / <a href="https://www.ics.forth.gr" rel="noopener noreferrer" target="_blank">FORTH</a>',
-    info: "Bachelor's Thesis",
+    position: "Bachelor's Thesis",
+    info: '<a href="https://www.ics.forth.gr/discs/r" rel="noopener noreferrer" target="_blank">DSC</a> / <a href="https://www.ics.forth.gr" rel="noopener noreferrer" target="_blank">FORTH</a>',
   },
 ];
 
