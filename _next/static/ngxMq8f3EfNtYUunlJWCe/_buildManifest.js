f8fba5c7324aa6c70bf77ec0ab52386ce7d8e8ff
keybraker <keybraker@users.noreply.github.com>
2023-08-04 06:57:39 +0000

    Deploying to gh-pages from @ keybraker/keybraker.github.io@9b9b7683472254e1c8bb32a8286ed963fa2233c6 🚀

---
+++ _next/static/ngxMq8f3EfNtYUunlJWCe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/147-678d71529c2a254f.js","static/chunks/pages/index-9aef74c028812af4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
