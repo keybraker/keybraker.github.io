7998b826832473af8962da9aa76ac61f3e009ce5
Keybraker <iantsiakkas@gmail.com>
2025-11-08 13:26:18 +0200

    Refactor 404 page to include Head component for SEO and update meta tags in index page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,24 @@
 import React from "react"
 import Image from "next/image";
+import Head from "next/head";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="
+    <>
+      <Head>
+        <title>Page Not Found - Ioannis Tsiakkas</title>
+        <meta name="description" content="The page you are looking for could not be found." />
+      </Head>
+      <div className="flex flex-col items-center">
+        <h1 className="
         text-center mt-24
         text-5xl italic text-tsiakkas-dark dark:text-tsiakkas-light
       ">
-        Page not found
-      </h1>
-      <Image src="/assets/14.svg" width={1600} height={1600} alt="404 error" />
-    </div>
+          Page not found
+        </h1>
+        <Image src="/assets/14.svg" width={1600} height={1600} alt="404 error" />
+      </div>
+    </>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,7 +97,6 @@ export default function Home() {
 
         <link rel="icon" href="/favicon.ico" />
 
-        <meta name="title" content={description} />
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
